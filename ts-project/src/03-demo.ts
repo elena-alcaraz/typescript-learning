33839e6e1d73b7a1a094e571ea2554fa66c01a14
elena-alcaraz <elena.alcaraz.martin@gmail.com>
2024-10-21 08:52:30 +0200

    Función anónima autoejecutada

---
+++ ts-project/src/03-demo.ts
@@ -0,0 +1,16 @@
+(( ) => {
+  let myProductName = 'Product 1';
+  let myProductPrice = 123;
+
+  myProductName = 'change';
+  myProductName.toLowerCase();
+
+  myProductName.toFixed();
+
+  const myProductStock = 1000;
+  const myProductStock2 = 'Product 1';
+}) ();
+
+
+
+/* Esto nos avisará, ya que nos avisa de que estamos declarando estas variables en otro archivo. Para no tener este tipo de erorr, creamos una función anónima autoejecutada.  */
---
+++ ts-project/src/03-typing.ts
@@ -0,0 +1,14 @@
+let myProductName = 'Product 1';
+let myProductPrice = 123;
+
+myProductName = 'change';
+myProductName.toLowerCase();
+
+myProductName.toFixed();
+
+const myProductStock = 1000;
+const myProductStock2 = 'Product 1';
+
+
+/* Deduce los tipos por mi automáticamente. Queda asignado de forma inferida.
+En el caso de las constantes, ya no nos dice que es un number, nos dará directamente el valor, ya que no podemos cambiarlo. */
